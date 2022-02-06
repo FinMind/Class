5bd81f8c2ae40fa17acfd80b2d987780a4631081
YenChen <yenchen0416@gmail.com>
2022-02-06 21:11:35 +0800

    add: practice code

---
+++ courses/01_Building_a_distributed_system_and_monitoring_system_with_Python/chapter01/practice/TaiwanTpexStockMarginPurchaseShortSale.py
@@ -0,0 +1,28 @@
+# TODO: Add imports
+
+
+#TODO: need to find the url
+URL = ""
+
+
+#TODO: need to add header
+HEADER = {
+}
+
+#TODO: need to complete the crawler
+def crawler(para:typing.Dict[str, str]) -> pd.DataFrame:
+    '''
+    Steps:
+    1) Get the parameters
+    2) Send the request
+    3) Return dataframe data
+    '''
+    return data
+
+
+if __name__ == "__main__":
+    para = {
+        "crawler_date": "111/01/26",
+    }
+    data = crawler(para)
+    print(data)
