f52dc90c405bc440ecd175dbd129c3177826bb42
sam <samlin266118@gmail.com>
2022-02-19 11:09:59 +0800

    feat: add 3.4.1

--- courses/01_Building_a_distributed_system_and_monitoring_system_with_Python/chapter03/3.4/3.4.1/solution/financialdata/worker.py
+++ courses/01_Building_a_distributed_system_and_monitoring_system_with_Python/chapter03/3.4/3.4.1/solution/financialdata/worker.py
@@ -3,7 +3,7 @@ from celery import Celery
 app = Celery(
     "task",
     # 只包含 tasks.py 裡面的程式, 才會成功執行
-    include=["tasks"],
+    include=["financialdata.tasks"],
     # 連線到 rabbitmq,
     # pyamqp://user:password@localhost:5672/
     # 本書的帳號密碼都是 worker
